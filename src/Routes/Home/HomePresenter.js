0f4531996974e2d666ff7a5be1854d4a781692a5
sujin <rlatnwls777@gmail.com>
2021-04-09 02:30:16 +0900

    메인 화면 css 수정

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,17 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types"
 import Card from "../../Components/Card"
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 100px;
+`;
+
 
 const HomePresenter = ({ productItems }) => (
   <>
-  {productItems && productItems.map(item => (
-      <Card
-        key={item.id}
-        id={item.id}
-        imageUrl={item.coverImage}
-        title={item.title}
-      />
-    ))}
+  <Container>
+    {productItems && productItems.map(item => (
+        <Card
+          key={item.id}
+          id={item.id}
+          imageUrl={item.coverImage}
+          title={item.title}
+        />
+      ))}
+  </Container>
+
   </>
 );
 
