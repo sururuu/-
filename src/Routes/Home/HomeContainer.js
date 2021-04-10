5471878571a56232623c636db21c2a2af73087f1
sujin <rlatnwls777@gmail.com>
2021-04-11 01:18:36 +0900

    주석 삭제

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-// import HomePresenter from "./HomePresenter";
 import { productItems } from "../../data/productItems";
 import Card from "../../Components/Card"
 import styled from "styled-components";
