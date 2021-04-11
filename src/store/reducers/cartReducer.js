20b88240ec3f1f23bb08a5baab927ed14fdc98b3
sujin <rlatnwls777@gmail.com>
2021-04-12 03:24:18 +0900

    장바구니 갯수 제한

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -1,7 +1,13 @@
 const cartReducer = (state = [], action) => {
   switch (action.type) {
     case "ADD_ITEM":
-      return [...state,action.payload];
+      if (state.length >= 3) {
+        alert('장바구니는 최대 3개까지 담을 수 있습니다.')
+        return [...state]
+      }
+      else {
+        return [...state,action.payload];
+      }
     case "DELETE_ITEM":
       return state.filter(item => item.id !== action.payload.id)
     default:
